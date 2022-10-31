a21cf3f5e4534ad9bde16ed1a224a47d25b5fcc1
yossavorn <yossavorn@gmail.com>
2022-10-31 18:48:22 +0700

    Deploy GH page

--- src/components/FooterForm.js
+++ src/components/FooterForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./FooterForm.css";
 const FooterForm = () => {
-    console.log("Kuy");
+    
   return (
     <div className="footer">
       <h2 className="footer__content">Made by React.JS, Yossavorn Iangleard </h2>
